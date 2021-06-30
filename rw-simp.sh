0d1b555696c6a89408af06684d3286d6f42f465c
Sidhartha Mazumder <sidmazumder8@gmail.com>
2021-07-01 01:07:36 +0530

    Uploading Files

---
+++ rw-simp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
+shuf -r $dir/rw.txt | head -n1 |figlet -f "3d" -t | lolcat 
+shuf -r $dir/rw.txt | head -n1 |figlet -f "3d" -t | lolcat
