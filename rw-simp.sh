99816142fae2aac0ee1fa57aedb2b68d45015a78
Sidhartha Mazumder <sidmazumder8@gmail.com>
2021-07-04 01:21:51 +0530

    Syntax

--- rw-simp.sh
+++ rw-simp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && shuf -r $dir/rw.txt | head -n1 | figlet -f "3d" -t | lolcat && shuf -r $dir/rw.txt | head -n1 | figlet -f "3d" -t | lolcat
+readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && shuf -r "$dir/rw.txt" | head -n1 | figlet -f "3d" -t | lolcat && shuf -r "$dir/rw.txt" | head -n1 | figlet -f "3d" -t | lolcat
