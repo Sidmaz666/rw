2fe53f63d480fabcece09b0083961c1aba126d7e
Sidhartha Mazumder <sidmazumder8@gmail.com>
2021-07-03 18:40:08 +0530

    One Line

--- rw-simp.sh
+++ rw-simp.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
-readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)";
-shuf -r $dir/rw.txt | head -n1 |figlet -f "3d" -t | lolcat && shuf -r $dir/rw.txt | head -n1 |figlet -f "3d" -t | lolcat
+#!/bin/bash
+readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && shuf -r $dir/rw.txt | head -n1 |figlet -f "3d" -t | lolcat && shuf -r $dir/rw.txt | head -n1 |figlet -f "3d" -t | lolcat
