4e59619416f312831ce86d2e2a7c3c01b52dd65d
Sidmaz666 <sidmazumder8@gmal.com>
2021-09-20 14:14:47 +0530

    Happy Update

--- rw-simp.sh
+++ rw-simp.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" && shuf -r "$dir/rw.txt" | head -n1 | figlet -f "3d" -t | lolcat && shuf -r "$dir/rw.txt" | head -n1 | figlet -f "3d" -t | lolcat
+readonly dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" 
+do="$(shuf -r "$dir/rw.txt" | head -n1 )"
+	dn="$(shuf -r "$dir/rw.txt" | head -n1)"
+		echo "["$do"] ["$dn"]" | figlet -f "3d" -t | nms -a -s | lolcat -a -d 1 -s 8000
